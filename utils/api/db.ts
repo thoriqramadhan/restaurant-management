c1b9191c7232732fa0d9ab566dabdf0c541b30e4
thoriqramadhan <respect.thoriq@gmail.com>
2025-06-11 13:34:46 +0700

    feat: add utils

---
+++ utils/api/db.ts
@@ -0,0 +1,15 @@
+import { fetchJS } from "../fetch";
+
+interface GetRoleResponse {
+    role: string
+}
+export async function getRole(email: string) : Promise<{status: number , role?: GetRoleResponse , msg?: string}>{
+    try {
+        const roleRes = await fetchJS(`/api/role?email=${email}`)    
+        const role = await roleRes.json()
+        return {status: 200 , role}
+    } catch (error) {
+        console.log(error);
+        return {status: 400 , msg: 'Failed to get role'}
+    }
+}

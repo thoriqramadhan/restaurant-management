16da172925950a1b072dfc9623577a488d94e3c2
thoriqramadhan <respect.thoriq@gmail.com>
2025-06-16 09:54:04 +0700

    feat: add product with images and existing images logic

--- components/client/ProductPageComponent.tsx
+++ components/client/ProductPageComponent.tsx
@@ -1,14 +1,15 @@
 "use client";
-import { Category, Product } from "@/types/database";
+import { Category, ImageDB, Product } from "@/types/database";
 import { addProduct } from "@/utils/actions/product";
 import { cn } from "@/utils/cn";
 import { FormatToIDR } from "@/utils/formatter";
-import { Funnel, Plus, Search, X } from "lucide-react";
+import { Funnel, Plus, Search, Upload, UtensilsCrossed, X } from "lucide-react";
 import { Ellipsis } from "lucide-react";
 import Image from "next/image";
 import {
   ChangeEvent,
   Dispatch,
+  FormEvent,
   SetStateAction,
   useEffect,
   useRef,
@@ -19,13 +20,14 @@ import React from "react";
 interface ProductsProps {
   initialProducts: Product[];
   initialCategories: Category[];
+  existingPhotos: ImageDB[];
 }
 export function Products({
   initialProducts,
   initialCategories,
+  existingPhotos,
 }: ProductsProps) {
   const [isAddProduct, setIsAddProduct] = useState(false);
-console.log(initialProducts)
   return (
     <>
       <section className="w-full h-fit">
@@ -59,25 +61,39 @@ console.log(initialProducts)
           </div>
         </div>
         <div className="w-full flex flex-wrap">
-          <div className="w-full h-fit mt-5 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-x-3">
-            {initialProducts.length > 0 && initialProducts.map((product , index) => <ProductCard key={index} name={product.name} price={String(product.price)} category={product.category} product_url={product.product_url}/>)}
+          <div className="w-full h-fit mt-5 grid grid-cols-2 gap-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+            {initialProducts.length > 0 &&
+              initialProducts.map((product, index) => (
+                <ProductCard
+                  key={index}
+                  name={product.name}
+                  price={String(product.price)}
+                  category={product.category}
+                  product_url={product.product_url}
+                />
+              ))}
           </div>
         </div>
       </section>
       <AddProducts
         productStateObj={{ isAddProduct, setIsAddProduct }}
-        initialDatas={{ initialProducts, initialCategories }}
+        initialDatas={{ initialProducts, initialCategories, existingPhotos }}
       />
     </>
   );
 }
 interface ProductCardProps {
-  name: string,
-  price: string,
-  category: string,
-  product_url: string
+  name: string;
+  price: string;
+  category: string;
+  product_url: string;
 }
-export function ProductCard({name , price , category, product_url} : ProductCardProps) {
+export function ProductCard({
+  name,
+  price,
+  category,
+  product_url,
+}: ProductCardProps) {
   const [isOpen, setIsOpen] = useState(false);
   const ref = useRef<HTMLDivElement>(null);
   useEffect(() => {
@@ -91,12 +107,27 @@ export function ProductCard({name , price , category, product_url} : ProductCard
   });
   return (
     <div className="h-[230px] lg:h-[250px] bg-white rounded-xl p-3 text-black relative">
-      <div className="w-full h-[70%] bg-slate-300 relative overflow-hidden">
-        {product_url && <Image src={product_url} alt="product_img" layout="fill" objectFit="cover"/>}
+      {/* img */}
+      <div className="w-full h-[70%] bg-slate-300 relative overflow-hidden flex justify-center items-center rounded-md">
+        <UtensilsCrossed size={70} color="white" />
+        {product_url && (
+          <Image
+            src={product_url}
+            alt="product_img"
+            layout="fill"
+            objectFit="cover"
+          />
+        )}
       </div>
-      <h2 className="text-lg font-semibold mt-1 tracking-wide">{name}</h2>
+      {/* name */}
+      <h2 className="text-lg font-semibold mt-1 tracking-wide capitalize">
+        {name}
+      </h2>
+      {/* category */}
       <p className="text-sm capitalize">{category}</p>
+      {/* price */}
       <p className="text-sm font-thin">{FormatToIDR(Number(price))}</p>
+      {/* dropdown */}
       <div
         className="px-2 py-1 cursor-pointer bg-white select-none rounded-full border absolute top-2 right-2 border-slate-500 flex items-center justify-center"
         onClick={() => setIsOpen((prev) => !prev)}
@@ -126,17 +157,29 @@ export function AddProducts({
   initialDatas: ProductsProps;
 }) {
   const { isAddProduct, setIsAddProduct } = productStateObj;
-  const { initialCategories } = initialDatas;
+  const { initialCategories, existingPhotos } = initialDatas;
   const [imgSrc, setImgSrc] = useState("");
   const imgInputRef = useRef<HTMLInputElement>(null);
-  const [detail, setDetail] = useState({
+  const detailInitials = {
     name: "",
     price: 0,
     category: initialCategories[0].name,
-  });
-  function handleDetail(key: "name" | "price" | "category", value: string) {
+    imgId: 0,
+  };
+
+  const [detail, setDetail] = useState(detailInitials);
+  function handleDetail(
+    key: "name" | "price" | "category" | "imgId",
+    value: string
+  ) {
     if (key === "price" && (Number(value) < 0 || value.includes("-"))) return;
-    setDetail((prev) => ({ ...prev, [key]: value }));
+    if (key == "imgId") {
+      setImgSrc("");
+    }
+    setDetail((prev) => ({
+      ...prev,
+      [key]: key === "imgId" ? Number(value) : value,
+    }));
   }
   function handleImgPreview(event: ChangeEvent<HTMLInputElement>) {
     const target = event.target.files?.[0];
@@ -150,13 +193,32 @@ export function AddProducts({
 
     // if(target?.type !== ''){}
   }
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    const actionResponse = await addProduct(
+      new FormData(event.currentTarget as HTMLFormElement)
+    );
+    if (actionResponse.status != 200) {
+      alert(actionResponse.msg);
+      return;
+    }
+    setDetail(detailInitials);
+    if (imgInputRef.current) {
+      imgInputRef.current.value = "";
+    }
+    setImgSrc("");
+    alert(actionResponse.msg);
+  }
   useEffect(() => {
-    console.log(detail);
-  }, [detail]);
+    if (detail.imgId == 0) return;
+    const newImgObject = existingPhotos.find((img) => img.id === detail.imgId);
+
+    setImgSrc(newImgObject!.img_url);
+  }, [detail.imgId]);
   return (
-    <section
+    <section  
       className={cn(
-        `w-full fixed top-0 left-0 p-5 h-screen bg-white transition-300 translate-y-[5000px] text-black ${
+        `w-full fixed top-0 left-0 p-5 h-screen bg-white transition-300 translate-y-[5000px] text-black overflow-y-auto ${
           isAddProduct && "translate-y-0"
         }`
       )}
@@ -169,78 +231,115 @@ export function AddProducts({
         <X color="black" />
       </div>
       {/* ---------- */}
+      <div className="h-[405px] md:mt-[10%] lg:mt-[5%]">
       <h2 className="mt-[60px] text-2xl font-semibold">Add New Products</h2>
-      <form action={addProduct} className="grid grid-cols-1">
+      <form
+        onSubmit={handleSubmit}
+        className="flex gap-5 auto-cols-auto flex-wrap h-full"
+      >
         {/* img inputs */}
-        <div className="">
-          <h3 className="text-lg">Add Image</h3>
+        <div className="flex flex-col gap-2 w-full md:flex-1">
+          <h3 className="text-xl ">Add Image</h3>
+          {existingPhotos.length > 0 && (
+            <>
+              <label htmlFor="existingImgId" className="block">
+                Existing Img
+              </label>
+              <select
+                name="existingImgId"
+                className="border p-1 rounded-md"
+                id="existingImgId"
+                value={detail.imgId}
+                onChange={(e) => handleDetail("imgId", e.target.value)}
+              >
+                <option value={0}>None</option>
+                {existingPhotos.map((img, index) => (
+                  <option value={img.id} key={index}>
+                    {img.path}
+                  </option>
+                ))}
+              </select>
+            </>
+          )}
           {/* img preview */}
-          {imgSrc && (
-            <div className=" w-full md:w-[500px] h-[300px] relative bg-slate-600/20 backdrop-blur-md">
+          <label
+            htmlFor="imgInput"
+            className={cn('w-full lg:h-[400px] h-[300px] rounded-md overflow-hidden relative bg-slate-600/20 backdrop-blur-md flex justify-center items-center cursor-pointer' , imgSrc && 'bg-slate-100/30 cursor-auto')}
+          >
+            <Upload size={60} />
+            {imgSrc && (
               <Image
                 src={imgSrc}
                 alt="img_preview"
                 layout="fill"
                 objectFit="contain"
               />
-            </div>
+            )}
+          </label>
+          {detail.imgId == 0 && (
+            <input
+              type="file"
+              name="imgInput"
+              id="imgInput"
+              accept=".jpg,.png,.jpeg,.webp"
+              className="hidden"
+              ref={imgInputRef}
+              onChange={(e) => handleImgPreview(e)}
+            />
           )}
-          <input
-            type="file"
-            name="imgInput"
-            id="imgInput"
-            accept=".jpg,.png,.jpeg,.webp"
-            ref={imgInputRef}
-            onChange={(e) => handleImgPreview(e)}
-          />
         </div>
         {/* detail inputs */}
-        <div className="">
-          <section>
-            <label htmlFor="productName">Name</label>
-            <input
-              type="text"
-              name="productName"
-              id="productName"
-              value={detail.name}
-              onChange={(event) => handleDetail("name", event.target.value)}
-              className="block border"
-            />
-          </section>
-          <section>
-            <label htmlFor="productPrice" className="block">Price</label>
-            {detail.price > 0 && <p>{FormatToIDR(detail.price)}</p>}
-            <input
-              type="number"
-              name="productPrice"
-              id="productPrice"
-              value={detail.price}
-              className="opacity-0 "
-              onChange={(event) => handleDetail("price", event.target.value)}
-            />
-          </section>
+        <div className="flex flex-col justify-between h-full w-full md:flex-1 pb-5">
+          <span className="block w-full">
+            <section>
+              <label htmlFor="productName">Name</label>
+              <input
+                type="text"
+                name="productName"
+                id="productName"
+                value={detail.name}
+                onChange={(event) => handleDetail("name", event.target.value)}
+                className="block border"
+              />
+            </section>
+            <section>
+              <label htmlFor="productPrice" className="block">
+                Price
+              </label>
+              {detail.price > 0 && <p>{FormatToIDR(detail.price)}</p>}
+              <input
+                type="number"
+                name="productPrice"
+                id="productPrice"
+                value={detail.price}
+                className="opacity-0 "
+                onChange={(event) => handleDetail("price", event.target.value)}
+              />
+            </section>
 
-          <select
-            name="category"
-            id="category"
-            className="capitalize"
-            value={detail.category}
-            onChange={(e) => handleDetail("category", e.target.value)}
-          >
-            {initialCategories.map((category, index) => (
-              <option value={category.name} key={index} className="index">
-                {category.name}
-              </option>
-            ))}
-          </select>
+            <select
+              name="category"
+              id="category"
+              className="capitalize"
+              value={detail.category}
+              onChange={(e) => handleDetail("category", e.target.value)}
+            >
+              {initialCategories.map((category, index) => (
+                <option value={category.name} key={index} className="index">
+                  {category.name}
+                </option>
+              ))}
+            </select>
+          </span>
           <button
             type="submit"
-            className="block w-full bg-black text-white px-3 py-1 rounded-xl cursor-pointer"
+            className="block w-full h-[35px] bg-black text-white px-3 py-1 rounded-xl cursor-pointer"
           >
             Add
           </button>
         </div>
       </form>
+      </div>
     </section>
   );
 }
--- utils/actions/product.ts
+++ utils/actions/product.ts
@@ -3,22 +3,57 @@
 import { sql } from "../neon";
 import { supabaseServer } from "../supabase";
 
+export async function addProduct(
+  formData: FormData
+): Promise<{ status: number; msg: string }> {
+  const { productName, productPrice, category, imgInput, existingImgId } =
+    Object.fromEntries(formData) as {
+      productName: string;
+      productPrice: string;
+      category: string;
+      imgInput?: File;
+      existingImgId?: string;
+    };
+  try {
+    await sql`BEGIN`;
+    // img id for products
+    
+    let imgId = (existingImgId != '0' )? existingImgId : null
+    // if didnt use existing img
+    
+    if (!imgId && imgInput?.size != 0) {
+      if(!imgInput) throw new Error('Img input required!')
+      // create file path
+      const filePath = `products/${imgInput.name}`;
+      // upload img to bucket
+      await supabaseServer.storage.from("oms").upload(filePath, imgInput);
+      // get signed url
+      const { data, error: signUrlError } = await supabaseServer.storage
+        .from("oms")
+        .createSignedUrl(filePath, 60 * 60 * 24 * 365);
+      // on signed error throw error   
+      if (signUrlError)
+        throw new Error("Error creating signed url", {
+          cause: signUrlError.message,
+        });
+      // create new image col and returning id 
+      const imageResult = await sql.query("INSERT INTO images(path , img_url) VALUES($1 , $2) RETURNING id", [
+        filePath,
+        data?.signedUrl,
+      ]);
+      // set img id
+      imgId = imageResult[0].id
+    }
+    // create products
+    await sql.query('INSERT INTO products(name , price , category , img_id) VALUES ($1 ,$2 ,$3, $4)' , [productName.toLowerCase() , Number(productPrice) , category.toLowerCase() , imgId])
+    await sql`COMMIT`
+  } catch (error) {
+    console.log(error);
+    // error rollback
+    return { status: 400, msg: `Failed adding product : ${error}` };
+    await sql`ROLLBACK`;
+  }
+  // await sql.query('INSERT INTO products(name , price , category , img_id) VALUES ($1 ,$2 ,$3, $4)' , [productName , Number(productPrice) , category , img.id])
 
-export async function addProduct(formData: FormData) : Promise<{status: number , msg: string}>{
-  const { productName, productPrice, category, imgInput } = Object.fromEntries(
-    formData
-  ) as {
-    productName: string;
-    productPrice: string;
-    category: string;
-    imgInput: File;
-  };
-  const filePath = `products/${imgInput.name}`
-  const {error:imgError} = await supabaseServer.storage.from('oms').upload(filePath, imgInput ,{ upsert: true})
-  if(imgError) return {status: 400 , msg: imgError.message}
-  const {data , error: signUrlError } = await supabaseServer.storage.from('oms').createSignedUrl(filePath , 60 * 60 * 24 * 365)
-  if(!data) return {status: 400 , msg: signUrlError.message}
-  await sql.query('INSERT INTO products(name , price , category , product_url) VALUES ($1 ,$2 ,$3, $4)' , [productName , Number(productPrice) , category , data.signedUrl])
-
-  return {status: 200 , msg: 'Success adding product!'}
+  return { status: 200, msg: "Success adding product!" };
 }
